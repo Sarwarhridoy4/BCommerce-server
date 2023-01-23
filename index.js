ec7aee7568d7de621e54ed6ce0b0ab2ab170f874
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2023-01-23 14:40:30 +0600

    api for deleting product added

--- index.js
+++ index.js
@@ -21,11 +21,20 @@ async function run() {
     try {
         const UserCollection = client.db("DbUser").collection("UserCollection");
         const ProductCollections = client.db("DbUser").collection("ProductCollections");
+        const CartCollections = client.db("DbUser").collection("CartCollections");
         //add consumers while registering
         app.put('/customers', async (req, res) => {
             const user = req.body;
             console.log(user);
             const result = await UserCollection.insertOne(user);
+            console.log(result);
+            res.send(result);
+        });
+        //add product to cart
+        app.post('/cart', async (req, res) => {
+            const cartinfo = req.body;
+            console.log(cartinfo);
+            const result = await CartCollections.insertOne(cartinfo);
             console.log(result);
             res.send(result);
         });
@@ -36,17 +45,49 @@ async function run() {
             res.send(result);
             
         });
-
+        //geting individual product detailse
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
             const query = {}
             const allproduct = await ProductCollections.find(query).toArray();
-            const product = allproduct.filter(pro=>pro._id == id)
+            const product = allproduct.filter(pro => pro._id == id)
             console.log(product);
             res.send(product);
             
-        })
+        });
+        //Show All orders
+        app.get('/orders', async (req, res) => {
+            const query = {}
+            const result = await CartCollections.find(query).toArray();
+            res.send(result);
+            
+        });
+        //geting individual users cart
+        //get order customer and admin
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {}
+            const orders = await CartCollections.find(query).toArray();
+            const users = await UserCollection.findOne({ email: email });
+
+            if (users?.role === 'admin') {
+                res.send(orders)
+            }
+            else {
+                const order = orders?.filter(order => order.email === email)
+                res.send(order);
+            }
+        });
+
+        // delete a cart item
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await CartCollections.deleteOne(query);
+            res.send(result);
+        });
 
         
     }
